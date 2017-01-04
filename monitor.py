cff4295520d7de21477d1b6bee14904fb42328bb
alexlao <lao.alex97@gmail.com>
2017-01-04 17:35:00 -0600

    Created the email function. Assumes the script is using a gmail account to email the recipient.

---
+++ monitor.py
@@ -0,0 +1,30 @@
+import time
+import requests
+import smtplib
+from timeit import default_timer as timer
+
+start = timer()
+
+def alert_email(user, pwd, recipient, subject, body):
+    gmail_sender=user
+    gmail_senderpwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+        """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)  # start smtp server on port 587
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_sender, gmail_senderpwd)  # login to gmail server
+        server.sendmail(FROM, TO, message)  # actually perform sending of mail
+        server.close()  # end server
+        print
+        'successfully sent the mail'  # alert user mail was sent
+    except Exception as e:  # else tell user it failed and why (exception e)
+        print
+        "failed to send mail, " + str(e)
