fb272db7df8256a0520dc18034aca746f8a62c09
alexlao <lao.alex97@gmail.com>
2017-01-05 14:31:19 -0600

    Fully functioning. Email is sent to recipient as soon as change is detected. Documented so any user will understand the code. If using this script, there are unique fields of information that must be filled for the script to fully function.

--- monitor.py
+++ monitor.py
@@ -1,40 +1,78 @@
 import time
 import requests
 import smtplib
+
 from timeit import default_timer as timer
 
 start = timer()
 
+#Function to send the email to a recipient when a change on a site is made.
+#When using this script, the user must update the server being used with the new emails respective information.
 def alert_email(user, pwd, recipient, subject, body):
-    gmail_sender=user
-    gmail_senderpwd = pwd
+    sender = user
+    senderpwd = pwd
     FROM = user
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
 
-    # Prepare actual message
+    # Makes the message object
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
         """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    #Tries to send an email to a recipient. If any error is thrown when attempting, it is caught.
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)  # start smtp server on port 587
+        server = smtplib.SMTP("mail.example.com", 26) #establishes connection to server
         server.ehlo()
         server.starttls()
-        server.login(gmail_sender, gmail_senderpwd)  # login to gmail server
+        server.login(sender, senderpwd)  # login to gmail server
         server.sendmail(FROM, TO, message)  # actually perform sending of mail
         server.close()  # end server
         print 'successfully sent the mail'  # alert user mail was sent
     except Exception as e:  # else tell user it failed and why (exception e)
         print "failed to send mail, " + str(e)
 
-
+#Main method runs indefinitely, until the user ends the script.
+#Checks if page has been updated
 def main():
+    with requests.Session() as c:
+        url = "http://www.ExampleAddress.com/" #url to be examined
+        refresh_time = 5
 
-    with requests.Session() as c
-        url = "https://www.mrporter.com/en-us/mens/adidas_originals/gazelle-suede-sneakers/721301?ppv=2"
-        refresh_time = 120
-        user = "alexlaodevtests@gmail.com"
-        pwd = "mxoi mpdo vmst engj"
-        recipient = "lao.alex97@gmail.com"
+        user = "sender@example.com" #Username of sender
+        pwd = "examplePWD!" #Password of sender
+
+        recipient = "example@example.com" #Recipient of email when update is made
         subject = "Site change"
-        body = "Change At" + str(url)
+        body = "Change At " + str(url)
+
+        page1=c.get(url) #retrieves information of the page for the first time
+
+        time.sleep(refresh_time)
+
+        page2=c.get(url) #after a wait, it retrieves information of a page again
+
+        #if pages are the same, nothing happens
+        if page1.content == page2.content:
+            end = timer()
+            if((end-start))>=60:
+                timeMinutes = (end-start)/60
+                print "No change detected @ " + str(url) + " Elapsed time: " + str(timeMinutes) +" minutes"
+            else:
+                print "No change detected @ " + str(url) + " Elapsed time: " + str((end-start)) + " seconds"
+
+        #if pages are different, then an email is sent to the recipient
+        else:
+            end = timer()
+            if int((end-start))>=60:
+                timeMinutes = (end-start)/60
+                print "Change Detected @ " + str(url) + " Elapsed time " + str(timeMinutes)
+            else:
+                print "Change Detected @ " + str(url) + " Elapsed Time " + str((end-start))
+
+            alert_email(user, pwd, recipient, subject, body)
+
+        page2 = None
+        main()
+
+if __name__ == "__main__":
+    main()
